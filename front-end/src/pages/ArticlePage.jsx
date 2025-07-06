31cdee3c56c6f4def061e01b8bdde9a9cc165de2
faizanalim <faizan.23@hotmail.com>
2025-07-06 19:51:50 +0500

    Added an upvote button

--- front-end/src/pages/ArticlePage.jsx
+++ front-end/src/pages/ArticlePage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useParams, useLoaderData } from "react-router-dom";
 import articles from "../article-content";
 import axios from 'axios';
@@ -6,12 +7,19 @@ import CommentsList from '../CommentsList';
 export default function ArticlePage() {
 
   const { name } = useParams();
-  const { upvotes, comments } = useLoaderData();
+  const { upvotes: initialUpvotes, comments } = useLoaderData();
+  const [upvotes, setUpvotes] = useState(initialUpvotes);
   const article = articles.find(x => x.name === name)
 
+  async function onUpvoteClicked() {
+    const response = await axios.post('/api/articles/' + name + '/upvote');
+    const updatedArticleData = response.data;
+    setUpvotes(updatedArticleData.upvotes);
+  }
   return (
     <>
       <h1>{article.title}</h1>
+      <button onClick={onUpvoteClicked}>Upvote</button>
       <p>This article has {upvotes} upvotes!</p>
       {article.content.map(p => <p key={p}>{p}</p>)}
       <CommentsList comments={comments} />
